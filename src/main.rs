8217a44a7c12ad411f49a462869bca3533402a3f
Thomas Wehmöller <connect.mordrag@gmx.de>
2023-04-26 18:50:16 +0200

    ♻️ Use new edge_list constructor in main

--- src/main.rs
+++ src/main.rs
@@ -21,18 +21,22 @@ fn graph_gross() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 fn prim() {
-    let edge_list = r#"7
-    1 2 0.2
-    1 3 0.3
-    1 4 0.3
-    2 3 0.4
-    2 5 0.3
-    3 4 0.5
-    3 5 0.1
-    4 6 0.7
-    5 6 0.8
-    6 0 0.9"#;
-    let edge_list = EdgeList::from_str(&edge_list).unwrap();
+    let edge_list = EdgeList::with(
+        [
+            (1, 2, 0.2),
+            (1, 3, 0.3),
+            (1, 4, 0.3),
+            (2, 3, 0.4),
+            (2, 5, 0.3),
+            (3, 4, 0.5),
+            (3, 5, 0.1),
+            (4, 6, 0.7),
+            (5, 6, 0.8),
+            (6, 0, 0.9),
+        ]
+        .into_iter(),
+        7,
+    );
     let graph = UndirectedAdjGraph::<usize, f64>::try_from(edge_list).unwrap();
     let total = graph.prim();
 
