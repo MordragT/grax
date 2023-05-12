7f800ab1561d45cd287fce7819af7385fd0469a4
Thomas Wehmöller <connect.mordrag@gmx.de>
2023-05-12 02:14:44 +0200

    🚧 Make nearest neighbor smarter with backtracking

--- src/graph/tsp.rs
+++ src/graph/tsp.rs
@@ -29,10 +29,10 @@ pub trait GraphTsp<
     + Sized
     + Clone
 {
-    fn nearest_neighbor(&self) -> GraphResult<W> {
+    fn nearest_neighbor(&self) -> Option<W> {
         match self.indices().next() {
             Some(start) => self._nearest_neighbor(start),
-            None => Ok(W::default()),
+            None => None,
         }
     }
 
@@ -97,12 +97,18 @@ impl<
 
 trait PrivateGraphTsp<
     N: PartialEq,
-    W: PartialOrd + Default + Add<W, Output = W> + AddAssign + Clone,
->: GraphTopology<N, W> + GraphAdjacentTopology<N, W> + GraphAccess<N, W> + GraphCompare<N, W>
+    W: PartialOrd + Default + Add<W, Output = W> + AddAssign + Clone + Sortable,
+>:
+    GraphTopology<N, W>
+    + GraphAdjacentTopology<N, W>
+    + GraphAccess<N, W>
+    + GraphCompare<N, W>
+    + PrivateGraphMst<N, W>
+    + Sized
 {
     fn _branch_bound(&self, start: NodeIndex) -> GraphResult<W> {
         let mut stack = Vec::new();
-        let mut total_cost = self._nearest_neighbor(start)?;
+        let mut total_cost = self._nearest_neighbor(start).unwrap();
 
         stack.push((W::default(), vec![start], vec![false; self.node_count()]));
 
@@ -138,64 +144,94 @@ trait PrivateGraphTsp<
         Ok(total_cost)
     }
 
-    fn _nearest_neighbor(&self, start: NodeIndex) -> GraphResult<W> {
-        let mut visited = vec![false; self.node_count()];
-        let mut total_weight = W::default();
-        let mut route = vec![(start, W::default())];
+    fn _nearest_neighbor(&self, start: NodeIndex) -> Option<W> {
+        #[derive(Default, Clone, Copy, Debug, PartialEq, Eq)]
+        enum Status {
+            Visited,
+            #[default]
+            Unvisited,
+            Diverged,
+        }
+
+        let mut states = vec![Status::default(); self.node_count()];
+        let mut path = vec![(start, W::default())];
         let mut prev = start;
 
-        while let Some((node, weight)) = route.last() {
-            visited[node.0] = true;
-            total_weight += weight.to_owned();
+        states[start.0] = Status::Visited;
 
-            if visited.iter().all(|v| *v) {
-                break;
-            }
+        while let Some((node, _)) = path.last() && path.len() < self.node_count() {
 
-            let mut min_edge: Option<Edge<W>> = None;
+            let mut min_edge = None;
 
-            for edge in self.adjacent_edges(*node) {
-                if !visited[edge.to.0] && edge.to != prev {
-                    if let Some(min) = &min_edge {
-                        if min.weight > *edge.weight {
-                            min_edge = Some(edge.into());
+            for EdgeRef { from:_, to, weight } in self.adjacent_edges(*node) {
+                if states[to.0] == Status::Unvisited && to != prev {
+                    if let Some((_, min_weight)) = &min_edge {
+                        if min_weight > weight {
+                            min_edge = Some((to, weight.to_owned()));
                         }
                     } else {
-                        min_edge = Some(edge.into());
+                        min_edge = Some((to, weight.to_owned()));
                     }
                 }
             }
 
             match min_edge {
-                Some(edge) => {
-                    route.push((edge.to, edge.weight));
-                    prev = edge.to;
+                Some((next, weight)) => {
+                    path.push((next, weight));
+                    states[next.0] = Status::Visited;
+                    prev = next;
                 }
                 None => {
-                    let dead_end = match route.pop() {
-                        Some((end, _)) => end,
-                        None => break,
-                    };
-                    visited[dead_end.0] = false;
-                    prev = dead_end;
+                    let open_end = path.iter().rposition(|(node, _)| {
+                        self.adjacent_indices(*node).any(|neigh| states[neigh.0] == Status::Unvisited)
+                    });
+
+                    if let Some(index) = open_end {
+                        let branch_point = path[index].0;
+
+                        if states[branch_point.0] == Status::Diverged {
+                            return None;
+                        } else {
+                            states[branch_point.0] = Status::Diverged;
+                        }
+                        let splitted_off = path.split_off(index + 1);
+                        for (node, _) in splitted_off.into_iter().rev() {
+                            states[node.0] = Status::Unvisited;
+                            prev = node;
+                        }
+                    } else {
+                        return None;
+                    }
                 }
             }
         }
 
-        if visited.into_iter().all(|visit| visit == true) {
-            if let Some(edge_index) = self.contains_edge(route.last().unwrap().0, start) {
-                total_weight += self.weight(edge_index).to_owned();
-                return Ok(total_weight);
-            }
+        assert!(states
+            .into_iter()
+            .all(|visit| visit == Status::Visited || visit == Status::Diverged));
+
+        match self.djikstra(prev, start) {
+            Some(weight) => path.push((start, weight)),
+            None => return None,
         }
-        Err(GraphError::NNAbort)
+
+        let total_weight = path.into_iter().fold(W::default(), |mut accu, (_, w)| {
+            accu += w;
+            accu
+        });
+
+        Some(total_weight)
     }
 }
 
 impl<
         N: PartialEq,
-        W: PartialOrd + Default + Add<W, Output = W> + AddAssign + Clone,
-        T: GraphTopology<N, W> + GraphAdjacentTopology<N, W> + GraphAccess<N, W> + GraphCompare<N, W>,
+        W: PartialOrd + Default + Add<W, Output = W> + AddAssign + Clone + Sortable,
+        T: GraphTopology<N, W>
+            + GraphAdjacentTopology<N, W>
+            + GraphAccess<N, W>
+            + GraphCompare<N, W>
+            + PrivateGraphMst<N, W>,
     > PrivateGraphTsp<N, W> for T
 {
 }
